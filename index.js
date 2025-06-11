c6fd40f28cd1f85125faf3c02902860bd4018100
Dennis-Kipkogei <kipkogeidennis05@gmail.com>
2025-06-11 13:33:17 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// Code your solutions 
+function writeCards(names, eventName) {
+      const messages = [];
+  for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  
+  return messages;
+
+
+}
+// count down
+function countDown(start) {
+      while (start >= 0) {
+    console.log(start);
+    start--;
+  }
+
+
+}
+
